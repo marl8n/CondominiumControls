ea24df030ff967aa8b76bf8e470f58ca786c4410
marl8n <midp515@gmail.com>
2021-03-11 17:57:13 -0600

    Created models Person and property

---
+++ models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumControls.models
+{
+    class Person
+    {
+        public string Dpi { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public Person()
+        {
+
+        }
+        public Person(string dpi, string name, string lastName)
+        {
+            this.Dpi = dpi;
+            this.Name = name;
+            this.LastName = lastName;
+        }
+    }
+}
---
+++ models/Property.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumControls.models
+{
+    class Property
+    {
+        public int Number { get; set; }
+        public string OwnerDpi { get; set; }
+        public double ManteinanceFee { get; set; }
+        public Property()
+        {
+
+        }
+        public Property(int number, string ownerDpi, double manteinanceFee)
+        {
+            this.Number = number;
+            this.OwnerDpi = ownerDpi;
+            this.ManteinanceFee = manteinanceFee;
+        }
+    }
+}
