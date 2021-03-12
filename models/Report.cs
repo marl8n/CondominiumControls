58b45a0d2d04d49f374ed6edcb7b158e9e1bb8bd
marl8n <midp515@gmail.com>
2021-03-11 18:59:14 -0600

    Created model report by person, report by property. Both clasess extends Report

---
+++ models/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumControls.models
+{
+    abstract class Report
+    {
+        public string OwnerDpi { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerLastName { get; set; }
+        public int PropertyNumber { get; set; }
+    }
+}
---
+++ models/ReportByProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumControls.models
+{
+    class ReportByProperty:Report
+    {
+        public double ManteinanceFee { get; set; }
+        public ReportByProperty()
+        {
+
+        }
+        public ReportByProperty(string ownerDpim, string ownerName, string ownerLastName, int propertyNumber, double mantainanceFee)
+        {
+            this.OwnerDpi = ownerDpim;
+            this.OwnerName = ownerName;
+            this.OwnerLastName = ownerLastName;
+            this.PropertyNumber = propertyNumber;
+            this.ManteinanceFee = mantainanceFee;
+        }
+    }
+}
