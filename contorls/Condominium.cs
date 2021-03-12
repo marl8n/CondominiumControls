e76ad29492cadca0dee3f35a5f81186f39c90289
marl8n <midp515@gmail.com>
2021-03-12 00:02:56 -0600

    Created condominium and reporter that can make reports by property

---
+++ contorls/Condominium.cs
@@ -0,0 +1,20 @@
+﻿using CondominiumControls.models;
+using CondominiumControls.persistance.person;
+using CondominiumControls.persistance.property;
+using CondominiumControls.persistance.reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumControls.contorls
+{
+    class Condominium
+    {
+        public static List<Person> People = JsonToPersons.GetPersonsOrNewIEnumerableOfPersons().ToList();
+        public static List<Property> Properties = JsonToProperties.GetPropertiesOrIEnumerableOfProperty().ToList();
+        public static List<Report> ReportsByPerson = JsonToReports.GetReportsOrIEnumerableOfReports(true).ToList();
+        public static List<Report> ReportsByProperty = JsonToReports.GetReportsOrIEnumerableOfReports(false).ToList();
+    }
+}
---
+++ reports/reporter.cs
@@ -0,0 +1,34 @@
+﻿using CondominiumControls.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumControls.reports
+{
+    class reporter
+    {
+        public static IEnumerable<ReportByProperty> MakeReportsByProperty(IEnumerable<Person> people, IEnumerable<Property> properties)
+        {
+            var reportsByProperty = new List<ReportByProperty>();
+            foreach ( var person in people )
+            {
+                var propertiesOwned = properties.Where(property => property.OwnerDpi.Equals(person.Dpi) ).ToList();
+                foreach ( var property in propertiesOwned )
+                {
+                    reportsByProperty.Add(
+                        new ReportByProperty(
+                            person.Dpi,
+                            person.Name,
+                            person.LastName,
+                            property.Number,
+                            property.ManteinanceFee
+                            )
+                        );
+                }
+            }
+            return reportsByProperty;
+        }
+    }
+}
