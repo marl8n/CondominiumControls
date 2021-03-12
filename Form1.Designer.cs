0cfef11619f3b719826dde48ca3d6e7ac7abde12
marl8n <midp515@gmail.com>
2021-03-12 00:47:24 -0600

    added sort to the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,101 @@ namespace CondominiumControls
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOrderByFee = new System.Windows.Forms.Button();
+            this.btnMostProperties = new System.Windows.Forms.Button();
+            this.btnByMantainanceTop = new System.Windows.Forms.Button();
+            this.btnOrderByMaintainanceBottom = new System.Windows.Forms.Button();
+            this.btnMostMantainanceFee = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(775, 395);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnOrderByFee
+            // 
+            this.btnOrderByFee.Location = new System.Drawing.Point(41, 13);
+            this.btnOrderByFee.Name = "btnOrderByFee";
+            this.btnOrderByFee.Size = new System.Drawing.Size(108, 23);
+            this.btnOrderByFee.TabIndex = 1;
+            this.btnOrderByFee.Text = "Order by fee";
+            this.btnOrderByFee.UseVisualStyleBackColor = true;
+            this.btnOrderByFee.Click += new System.EventHandler(this.btnOrderByFee_Click);
+            // 
+            // btnMostProperties
+            // 
+            this.btnMostProperties.Location = new System.Drawing.Point(174, 13);
+            this.btnMostProperties.Name = "btnMostProperties";
+            this.btnMostProperties.Size = new System.Drawing.Size(123, 23);
+            this.btnMostProperties.TabIndex = 2;
+            this.btnMostProperties.Text = "Most Properties";
+            this.btnMostProperties.UseVisualStyleBackColor = true;
+            this.btnMostProperties.Click += new System.EventHandler(this.btnMostProperties_Click);
+            // 
+            // btnByMantainanceTop
+            // 
+            this.btnByMantainanceTop.Location = new System.Drawing.Point(319, 13);
+            this.btnByMantainanceTop.Name = "btnByMantainanceTop";
+            this.btnByMantainanceTop.Size = new System.Drawing.Size(145, 23);
+            this.btnByMantainanceTop.TabIndex = 3;
+            this.btnByMantainanceTop.Text = "By mantainance ⬆";
+            this.btnByMantainanceTop.UseVisualStyleBackColor = true;
+            this.btnByMantainanceTop.Click += new System.EventHandler(this.btnByMantainanceTop_Click);
+            // 
+            // btnOrderByMaintainanceBottom
+            // 
+            this.btnOrderByMaintainanceBottom.Location = new System.Drawing.Point(481, 13);
+            this.btnOrderByMaintainanceBottom.Name = "btnOrderByMaintainanceBottom";
+            this.btnOrderByMaintainanceBottom.Size = new System.Drawing.Size(136, 23);
+            this.btnOrderByMaintainanceBottom.TabIndex = 4;
+            this.btnOrderByMaintainanceBottom.Text = "By mantainance ⬇";
+            this.btnOrderByMaintainanceBottom.UseVisualStyleBackColor = true;
+            this.btnOrderByMaintainanceBottom.Click += new System.EventHandler(this.btnOrderByMaintainanceBottom_Click);
+            // 
+            // btnMostMantainanceFee
+            // 
+            this.btnMostMantainanceFee.Location = new System.Drawing.Point(624, 12);
+            this.btnMostMantainanceFee.Name = "btnMostMantainanceFee";
+            this.btnMostMantainanceFee.Size = new System.Drawing.Size(145, 23);
+            this.btnMostMantainanceFee.TabIndex = 6;
+            this.btnMostMantainanceFee.Text = "Most Mantainance";
+            this.btnMostMantainanceFee.UseVisualStyleBackColor = true;
+            this.btnMostMantainanceFee.Click += new System.EventHandler(this.btnMostMantainanceFee_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMostMantainanceFee);
+            this.Controls.Add(this.btnOrderByMaintainanceBottom);
+            this.Controls.Add(this.btnByMantainanceTop);
+            this.Controls.Add(this.btnMostProperties);
+            this.Controls.Add(this.btnOrderByFee);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOrderByFee;
+        private System.Windows.Forms.Button btnMostProperties;
+        private System.Windows.Forms.Button btnByMantainanceTop;
+        private System.Windows.Forms.Button btnOrderByMaintainanceBottom;
+        private System.Windows.Forms.Button btnMostMantainanceFee;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using static CondominiumControls.contorls.Condominium;
+
 namespace CondominiumControls
 {
     public partial class Form1 : Form
@@ -15,6 +17,31 @@ namespace CondominiumControls
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOrderByFee_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ReportsByProperty.OrderBy(p => p.ManteinanceFee);
+        }
+
+        private void btnMostProperties_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ReportsByPerson.OrderBy(p => p.QuantityOfProperties);
+        }
+
+        private void btnByMantainanceTop_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ReportsByProperty.OrderBy(p => p.ManteinanceFee);
+        }
+
+        private void btnOrderByMaintainanceBottom_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ReportsByProperty.OrderBy(p => p.ManteinanceFee).Reverse();
+        }
+
+        private void btnMostMantainanceFee_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ReportsByPerson.OrderBy(p => p.TotalManteinanceFee);
         }
     }
 }
